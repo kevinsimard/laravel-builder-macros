69ef454a8ff05d534a68522eb01f72b4364895f6
Kevin Simard <kev.simard@gmail.com>
2016-11-10 12:29:40 -0500

    Replace single quotes by double quotes

--- src/Providers/OrderByRandomServiceProvider.php
+++ src/Providers/OrderByRandomServiceProvider.php
@@ -12,12 +12,12 @@ class OrderByRandomServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Builder::macro('orderByRandom', function () {
+        Builder::macro("orderByRandom", function () {
             $randomFunctions = [
-                'mysql' => 'RAND()',
-                'pgsql' => 'RANDOM()',
-                'sqlite' => 'RANDOM()',
-                'sqlsrv' => 'NEWID()',
+                "mysql" => "RAND()",
+                "pgsql" => "RANDOM()",
+                "sqlite" => "RANDOM()",
+                "sqlsrv" => "NEWID()",
             ];
 
             $driver = $this->getConnection()->getDriverName();
